57565b79fa0e37978bea26cfa394e76ad70a1983
ktran <ktran@paramountcommerce.com>
2024-02-05 23:08:50 -0500

    Init sec project

---
+++ src/main/java/io/login/sec/domain/Account.java
@@ -0,0 +1,59 @@
+package io.login.sec.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "account", indexes = {
+        @Index(name = "idx_account_email", columnList = "email")
+})
+public class Account {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @JsonIgnore
+    @NotNull
+    private String password;
+
+    @JsonIgnore
+    @NotNull
+    @Column(name = "encoded_password", nullable = false)
+    private String encodedPassword;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "expired_at")
+    private Instant expiredAt;
+
+    @Column(name = "locked_at")
+    private Instant lockedAt;
+
+    @Column(name = "credentials_expired_at")
+    private Instant credentialsExpiredAt;
+
+    @Column(name = "enabled")
+    private Boolean enabled;
+}
