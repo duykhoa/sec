348c230008265b197a95ca5c4f2cb7eb1966f3bc
ktran <ktran@paramountcommerce.com>
2024-02-08 06:36:02 -0500

    Enable application security (authorization)

--- src/main/java/io/login/sec/configuration/SecConfiguration.java
+++ src/main/java/io/login/sec/configuration/SecConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,10 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableGlobalMethodSecurity(
-        prePostEnabled = true,
-        securedEnabled = true
-)
+@EnableMethodSecurity()
 public class SecConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -25,7 +23,7 @@ public class SecConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/create-test-user")
+                        .requestMatchers("/demo/default")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
